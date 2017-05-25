bfaa8d0b2cd17cc2362326d4cff30af9b4b98e3f
jhoangomez013 <jsgomez013@gmail.com>
2017-05-24 21:56:13 -0500

    Insert 'Back' en la funcionalidad Paseo Virtual

--- app/src/main/java/co/edu/uniquindio/android/project/biblioteca/packages/actividades/UnityPlayerNativeActivity.java
+++ app/src/main/java/co/edu/uniquindio/android/project/biblioteca/packages/actividades/UnityPlayerNativeActivity.java
@@ -1,7 +1,23 @@
 package co.edu.uniquindio.android.project.biblioteca.packages.actividades;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InterruptedIOException;
+
+import co.edu.uniquindio.android.project.biblioteca.packagesAR.R;
+
+import static co.edu.uniquindio.android.project.biblioteca.packagesAR.R.*;
 
 /**
  * Actividad perteneciente al codigo generado por el proceo realizado en Unity
@@ -18,16 +34,21 @@ public class UnityPlayerNativeActivity extends UnityPlayerActivity {
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.w("Unity", "UnityPlayerNativeActivity has been deprecated, please update your AndroidManifest to use UnityPlayerActivity instead");
         super.onCreate(savedInstanceState);
-    }
+            // Add a Button over the Unity view
+            ImageButton sampleButton = new ImageButton(UnityPlayerNativeActivity.this);
+            sampleButton.getBackground().setAlpha(0);
+            sampleButton.setBackgroundResource(0);
+            sampleButton.setImageResource(drawable.abc_ic_ab_back_mtrl_am_alpha);
+            sampleButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    onDestroy();
 
-    /**
-     * Metodo que genera la acción atras
-     */
-    @Override
-    public void onBackPressed() {  // Añade más funciones si fuese necesario
-        super.onBackPressed();  // Invoca al método
-    }
+                }
+            });
+            mUnityPlayer.addView(sampleButton, new ViewGroup.LayoutParams( ViewGroup.LayoutParams.WRAP_CONTENT,  ViewGroup.LayoutParams.WRAP_CONTENT));
+            sampleButton.bringToFront();
 
+    }
 }
