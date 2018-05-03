2b8dacda3511a1f9f7e2296834967f124a1492b7
Sebastián A <sebas199500@hotmail.com>
2018-05-02 21:56:09 -0500

    change about

--- app/src/main/java/co/edu/uniquindio/android/project/biblioteca/packages/actividades/AcercaDeActivity.java
+++ app/src/main/java/co/edu/uniquindio/android/project/biblioteca/packages/actividades/AcercaDeActivity.java
@@ -3,6 +3,9 @@ package co.edu.uniquindio.android.project.biblioteca.packages.actividades;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -15,14 +18,8 @@ import co.edu.uniquindio.android.project.biblioteca.packagesAR.R;
  */
 public class AcercaDeActivity extends AppCompatActivity {
 
-    @BindView(R.id.autor_2)
-    public TextView autor_2;
-    @BindView(R.id.info)
-    public TextView info;
-    @BindView(R.id.creada_anio)
-    public TextView creada_anio;
-    @BindView(R.id.version)
-    public TextView version;
+    //Variable Web view
+    private WebView WebView;
 
     /**
      * aca se crea la vista del layout correspondiente a la clase.
@@ -32,21 +29,17 @@ public class AcercaDeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acerca_de);
         ButterKnife.bind(this);
+
+        WebView = (WebView) this.findViewById(R.id.about_view);
+        WebView.loadUrl("file:///android_asset/about.html");
+        WebView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = WebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
         atras();
         getSupportActionBar().setTitle("Acerca De");
-        asignarElementosLayout();
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
-    /**
-     * Elementos mostrados en el layout
-     */
-    private void asignarElementosLayout() {
-        info.setText(R.string.info);
-        version.setText(R.string.version);
-        autor_2.setText(R.string.autor_2);
-        creada_anio.setText(R.string.creada_anio);
-    }
 
     /**
      * Se adiciona Accion atras en el Action Bar
